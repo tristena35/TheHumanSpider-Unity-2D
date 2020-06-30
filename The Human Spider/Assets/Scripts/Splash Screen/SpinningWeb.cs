241ec2e0ade568acbff6372825730704665fdc58
Tristen Aguilar <tristenaguilar24@gmail.com>
2020-06-30 19:35:38 -0400

    Added Game and Control Screens

---
+++ The Human Spider/Assets/Scripts/Splash Screen/SpinningWeb.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningWeb : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        RotateWeb();
+    }
+
+    void RotateWeb()
+    {
+        transform.Rotate( Vector3.forward * Time.deltaTime * 180 );
+    }
+}
---
+++ The Human Spider/Assets/Scripts/Used In Several Scenes/SceneLoader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [Header("Times")]
+    [SerializeField] float timeToStartGame = 15f;
+    [SerializeField] float timeToMainMenu = 7.5f;
+
+    [Header("Scene Number")]
+    [SerializeField] int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Grabs the number of the current scene
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // If we are at the splash screen, do coroutine
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine( SplashToStart() );
+        }
+
+        // If on loading screen, load game
+        if (currentSceneIndex == 2)
+        {
+            StartCoroutine( LoadToGame() );
+        }
+    }
+
+    void LoadMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator SplashToStart()
+    {
+        yield return new WaitForSeconds(timeToMainMenu);
+        LoadMainMenu();
+    }
+
+    IEnumerator LoadToGame()
+    {
+        yield return new WaitForSeconds(timeToStartGame);
+        LoadGame();
+    }
+}
