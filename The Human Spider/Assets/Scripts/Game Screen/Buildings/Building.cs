f5c81a0fca88884f01252d1dfb993bc07b398d00
Tristen Aguilar <tristenaguilar24@gmail.com>
2020-07-01 16:31:12 -0400

    Updated Game Screen

---
+++ The Human Spider/Assets/Scripts/Game Screen/Buildings/Building.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    Vector2 buildingMovement = new Vector2( -0.2f, 0 );
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Time.deltaTime * -5f, 0, 0);
+
+    }
+}
