9a5ab817fcaaecc2a0ec4b2a5bd88567e16ab34b
Tristen Aguilar <tristenaguilar24@gmail.com>
2020-07-17 18:00:41 -0400

    Updated game screen

---
+++ The Human Spider/Assets/Scripts/Game Screen/Gameplay/ScoreKeeper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    bool startTracking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( startTracking )
+        {
+            ScoreTracking();
+        }
+    }
+
+    // Every second that 
+    void ScoreTracking()
+    {
+
+    }
+
+    // Call from start trigger IEnumerator
+    public void SetStartTracking()
+    {
+        startTracking = true;
+    }
+}
