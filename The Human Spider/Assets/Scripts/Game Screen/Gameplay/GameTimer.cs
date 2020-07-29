1f3b0d948c489360e8af2f82a99bb2d6b294ffb6
Tristen Aguilar <tristenaguilar24@gmail.com>
2020-07-28 23:16:14 -0400

    Added timer to game

---
+++ The Human Spider/Assets/Scripts/Game Screen/Gameplay/GameTimer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    public float currentTime = 0;
+    public bool timerIsRunning = false;
+    public float timeTillStart = 5f;
+    public TextMeshProUGUI timeText;
+
+    private void Start()
+    {
+        StartCoroutine( TimerToStart() );
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            currentTime += Time.deltaTime;
+            DisplayTime(currentTime);
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0}:{1:00}", minutes, seconds);
+    }
+
+    void StartTimer()
+    {
+        timerIsRunning = true;
+    }
+
+    IEnumerator TimerToStart()
+    {
+        yield return new WaitForSeconds(timeTillStart);
+        
+        StartTimer();
+    }
+}
